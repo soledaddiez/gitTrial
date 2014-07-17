fca0a32aafa7be632d8cfb2b66e244a2cf196a78
soledaddiez <diez.soledad@gmail.com>
2014-07-18 10:20:08 +1200

    asd

--- Trial/src/Trial.java
+++ Trial/src/Trial.java
@@ -3,7 +3,7 @@ public class Trial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("aca tengo un cambio en el numero");
+		System.out.println("aca tengo un cambio en el otro numero");
 	}
 
 }

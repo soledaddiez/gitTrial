f99f5217462bddcc93d2fc738a6b8982a659af49
soledaddiez <diez.soledad@gmail.com>
2014-07-18 13:49:55 +1200

    asd

--- Trial/src/com/Trial.java
+++ Trial/src/com/Trial.java
@@ -5,6 +5,8 @@ public class Trial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sole Feature 07");
+		
+		System.out.println("Cambio en el develop");
 	}
 
 }

4be34f728cc73c7150e14de29f1c25ee0fed6ebc
soledaddiez <diez.soledad@gmail.com>
2014-07-18 13:27:14 +1200

    Changes

--- Trial/src/Trial.java
+++ Trial/src/Trial.java
@@ -3,7 +3,7 @@ public class Trial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sole Feature 01");
+		System.out.println("Sole Feature 07");
 	}
 
 }

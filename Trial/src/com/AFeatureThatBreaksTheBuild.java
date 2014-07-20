5e453200455cf9c0b069896d32db329c3a5a59a8
Arthur Koo <arthurh@findly.com>
2014-07-21 11:04:48 +1200

    Breaking featuer

---
+++ Trial/src/com/AFeatureThatBreaksTheBuild.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Trial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am a ... I dont know!!!");
+		
+		String a = "Esta es mi modificación para meter un code review acá.";
+		
+		System.out.println(a);
+	}
+
+}

781506413613301fc07b272bc1d16172212c7d0b
Isak Gilbert <isak.gilbert@gmail.com>
2014-07-21 11:13:26 +1200

    Output change

--- Trial/src/com/Trial.java
+++ Trial/src/com/Trial.java
@@ -4,7 +4,7 @@ public class Trial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I am a ... I dont know!!!");
+		System.out.println("I am a ... dumb I dont know!!!");
 		
 		String a = "Esta es mi modificación para meter un code review acá.";
 		

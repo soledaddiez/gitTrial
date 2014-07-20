65f803c505b1a84a4e1d935258dbdc9a0d91fb11
soledaddiez <diez.soledad@gmail.com>
2014-07-21 10:20:23 +1200

    para ver lo del pull request dinamico

--- Trial/src/com/Trial.java
+++ Trial/src/com/Trial.java
@@ -3,11 +3,7 @@ package com;
 public class Trial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Sole Feature 07");
-		
-		System.out.println("I am a conflictaaaa I AM PULLING REQUIEST USING SOURCE TREE");
-		
+
 		String a = "Esta es mi modificación para meter un code review acá.";
 		
 		System.out.println(a);

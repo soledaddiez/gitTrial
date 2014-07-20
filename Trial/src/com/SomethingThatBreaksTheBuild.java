f599ce0c3a2ad7fdbb4ffbae59d6ba502cab9edd
Arthur Koo <arthurh@findly.com>
2014-07-21 09:59:14 +1200

    Committed something that will break the build :D

---
+++ Trial/src/com/SomethingThatBreaksTheBuild.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Trial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sole Feature 07");
+		
+		System.out.println("I am a conflictaaaa I AM PULLING REQUIEST USING SOURCE TREE");
+		
+	}
+
+}

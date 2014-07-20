f8d4be4ae3932d8e15b3e19d294ae744ede164ce
soledaddiez <diez.soledad@gmail.com>
2014-07-21 09:50:32 +1200

    para ver lo del pull request dinamico

--- Trial/src/com/Trial.java
+++ Trial/src/com/Trial.java
@@ -8,6 +8,9 @@ public class Trial {
 		
 		System.out.println("I am a conflictaaaa I AM PULLING REQUIEST USING SOURCE TREE");
 		
+		String a = "Este es mi modificacion para meter un code review aca...";
+		
+		System.out.println(a);
 	}
 
 }

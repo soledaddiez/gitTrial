4b5b540245388eba9b711a7efd4baf83c6868fdf
Isak Gilbert <isak.gilbert@gmail.com>
2014-07-21 12:44:51 +1200

    Refactor New file

---
+++ Trial/src/com/TrialTest.java
@@ -0,0 +1,14 @@
+package com;
+
+public class TrialTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am a ... dumb I dont know!!! Another change"); // comment here
+		
+		String a = "Esta es mi modificación para meter un code review acá.";
+		
+		System.out.println(a);
+	}
+
+}
